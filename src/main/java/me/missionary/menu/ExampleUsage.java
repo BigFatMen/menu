93da2a360e24494f4a1a446758e2722a2b02202f
Missionary <missionarymc@gmail.com>
2018-03-26 11:40:08 -0700

    Add an example :memo:

--- src/main/java/me/missionary/menu/ExampleUsage.java
+++ src/main/java/me/missionary/menu/ExampleUsage.java
@@ -1,6 +1,8 @@
 package me.missionary.menu;
 
 import me.missionary.menu.button.Button;
+import me.missionary.menu.mask.IMask;
+import me.missionary.menu.mask.Mask2D;
 import me.missionary.menu.util.ItemBuilder;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -15,6 +17,8 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @since 2/21/2018
  */
 public class ExampleUsage extends JavaPlugin implements CommandExecutor {
+
+    private static final ItemBuilder MASK_FILLER = new ItemBuilder(Material.STAINED_GLASS_PANE).setDurability((short) 8).setName(ChatColor.WHITE + "");
 
     @Override
     public void onEnable() {
@@ -40,10 +44,15 @@ public class ExampleUsage extends JavaPlugin implements CommandExecutor {
 
         public void doSomething(Player player) {
             Menu menu = new Menu("Example Menu", 4);
-            menu.setItem(0, new Button(true, new ItemBuilder(Material.STICK).setName(ChatColor.LIGHT_PURPLE + "Stick"), (player1, button) -> {
+            IMask mask = new Mask2D.Builder(menu.getSize(), 9).apply("111111111") // Fills all the spaces with 1's with something which is applied later
+                    .nextRow().apply("100000001")
+                    .nextRow().apply("100000001")
+                    .nextRow().apply("111111111").build();
+            mask.forEach(integer -> menu.setItem(integer, new Button(false, MASK_FILLER))); // Fills all of the 1's with the MASK_FILLER
+            menu.setItem(12, new Button(true, new ItemBuilder(Material.STICK).setName(ChatColor.LIGHT_PURPLE + "Stick"), (player1, button) -> {
                 player1.sendMessage("You have clicked the Stick."); // Java 8 Functional Style
             }));
-            menu.setItem(1, new Button(true, new ItemBuilder(Material.EMERALD).setName(ChatColor.GREEN + "Emerald"), (player1, button) -> {
+            menu.setItem(13, new Button(true, new ItemBuilder(Material.EMERALD).setName(ChatColor.GREEN + "Emerald"), (player1, button) -> {
                 player1.sendMessage("Wow! You have clicked the Emerald.");
             }));
             menu.setStatic(true); // Make this menu never unregister, allows for re-use. default is false
